6b95b4f03cec7234ef3afee9285c1a6eb9408a1b
Cliff St John <cstjohn1990@gmail.com>
2025-01-25 13:08:51 -0500

    Homepage revamp

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,103 +1,385 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:async';
+import 'themes.dart';
+import 'create_proj+teams.dart';
+import 'compare_project.dart';
+import 'settings_page.dart';
+import 'team_invites.dart';
+import 'results_pan.dart';
+import 'main.dart';
+import 'edit_project.dart';
+
+List<String> navIcons2 = [
+  'assets/Home_Icon.png',
+  'assets/Add_Icon.png',
+  'assets/Compare_Icon.png',
+  'assets/Profile_Icon.png',
+];
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  Timer? _sessionTimer;
-  String? displayName; // To store the user's full name
+class _HomeScreenState extends State<HomePage> {
+  int selectedIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _startSessionTimer();
-    _fetchUserData(); // Fetch user data on page load
-  }
-
-  void _startSessionTimer() {
-    const sessionTimeout = Duration(minutes: 30); // Set inactivity timeout
-    _sessionTimer?.cancel();
-    _sessionTimer = Timer(sessionTimeout, _signOutUser);
-  }
-
-  void _resetTimer() {
-    _startSessionTimer(); // Reset the session timer
-  }
-
-  Future<void> _fetchUserData() async {
-    try {
-      User? user = _auth.currentUser;
-      if (user != null) {
-        // Fetch user data from Firestore
-        final DocumentSnapshot userDoc =
-            await _firestore.collection('users').doc(user.uid).get();
-        if (userDoc.exists) {
-          setState(() {
-            displayName = userDoc['fullName']; // Fetch user's full name
-          });
-        } else {
-          print('User data not found in Firestore.');
-        }
-      } else {
-        print('No authenticated user found.');
-      }
-    } catch (e) {
-      print('Error fetching user data: $e');
-    }
-  }
-
-  void _signOutUser() async {
-    await _auth.signOut();
-    Navigator.pushReplacementNamed(context, '/'); // Redirect to login page
+  void onNavItemTapped(int index) {
+    setState(() {
+      selectedIndex = index; // Update the selected index
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _resetTimer, // Reset timer on user interaction
-      onPanUpdate: (_) => _resetTimer(), // Track gestures
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: _signOutUser, // Log out the user
-            ),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Welcome to the Home Page!',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 20),
-              displayName != null
-                  ? Text(
-                      'Hello, $displayName', // Display user's full name
-                      style: TextStyle(fontSize: 18),
-                    )
-                  : CircularProgressIndicator(), // Show loading spinner until data is fetched
+    return Scaffold(
+      body: Stack(
+        children: [
+          IndexedStack(
+            index: selectedIndex,
+            children: [
+              // Screens for each tab
+              _buildHomeContent(),
+              CreateProjectAndTeamsPage(),
+              ProjectComparisonPage(),
+              SettingsPage(),
             ],
           ),
+          // Bottom Navigation Bar
+          Positioned(
+            bottom: 50,
+            left: 20,
+            right: 20,
+            child: _navBar(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHomeContent() {
+    return //Scrollable content
+        SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // P2BP Logo centered at the top
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Image.asset(
+                      'assets/P2BP_Logo.png',
+                      width: 40,
+                      height: 40,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        // Notification button
+                        IconButton(
+                          icon: Image.asset('assets/bell-03.png'),
+                          onPressed: () {
+                            // Handle notification button action
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const HomePage()
+                                )
+                              );
+                          },
+                          iconSize: 24,
+                        ),
+                        // Teams Button
+                        IconButton(
+                          icon: const Icon(Icons.group),
+                          color: const Color(0xFF0A2A88),
+                          onPressed: () {
+                            // Navigate to Teams/Invites screen
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      const TeamsAndInvitesPage(),
+                                ));
+                          },
+                          iconSize: 24,
+                        ),
+                      ],
+                    ),
+                  ),
+                  // "Hello, [user]" greeting, aligned to the left below the logo
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ShaderMask(
+                          shaderCallback: (bounds) {
+                            return defaultGrad.createShader(bounds);
+                          },
+                          child: const Text(
+                            'Hello,\nMichael',
+                            style: TextStyle(
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                height: 1.2 // Masked text with gradient
+                                ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // "Your Projects" label, aligned to the right of the screen"
+            Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ShaderMask(
+                    shaderCallback: (bounds) {
+                      return defaultGrad.createShader(bounds);
+                    },
+                    child: const Text(
+                      'Your Projects',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        height: 1.2,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Project Cards
+            Column(
+              children: [
+                // First Project Card
+                buildProjectCard(
+                  context,
+                  'assets/RedHouse.png',
+                  'Project Eola',
+                  'Team: Eola Design Group',
+                ),
+                const SizedBox(height: 20),
+                // Second Project Card
+                buildProjectCard(
+                  context,
+                  'assets/PinkHouse.png',
+                  'Project Neocity',
+                  'Team: New Horizons Placemakers',
+                ),
+                const SizedBox(height: 20),
+                // Third Project Card
+                buildProjectCard(
+                  context,
+                  'assets/RedHouse.png',
+                  'Project Knight Library',
+                  'Team: Lake Nona Design Group',
+                ),
+                const SizedBox(height: 150),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 
-  @override
-  void dispose() {
-    _sessionTimer?.cancel(); // Cancel the timer when the widget is disposed
-    super.dispose();
+  Widget buildProjectCard(
+    BuildContext context,
+    String bannerImage, // Image path for banner
+    String projectName, // Project name
+    String teamName, // Team name
+  ) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(12) // Match the container's corner radius
+          ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [Color(0xFF3874CB), Color(0xFF183769)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Banner image at the top
+            ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+              child: Image.asset(
+                bannerImage,
+                width: double.infinity,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            // Project details section
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Project name
+                  Text(
+                    projectName,
+                    style: const TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFFCC00),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  // Team name
+                  Text(
+                    teamName,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFFCC00),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Row with Edit and Results buttons in the bottom right corner
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10,
+                bottom: 10,
+                right: 10,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  // Edit Info button
+                  OutlinedButton(
+                    onPressed: () {
+                      // Handle navigation to Edit menu
+                      showEditProjectModalSheet(context);
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        color: Color(0xFFFFCC00),
+                        width: 2.0,
+                      ),
+                      foregroundColor: const Color(0xFFFFCC00),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Edit Info',
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  // Results button
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle navigation to Results menu
+                      showResultsModalSheet(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFFFCC00),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: const Text(
+                      'Results',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xFF1D4076),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _navBar() {
+    return Container(
+      height: 60,
+      margin: const EdgeInsets.only(right: 24, left: 24, bottom: 24),
+      decoration: BoxDecoration(
+        color: const Color(0xFF2F6DCF),
+        borderRadius: BorderRadius.circular(120),
+        boxShadow: [
+          BoxShadow(
+            color: Color.alphaBlend(
+              Colors.black.withAlpha(102), // 40% opacity for the shadow
+              Colors.transparent, // Transparent background blending
+            ),
+            blurRadius: 30,
+            spreadRadius: 5,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: navIcons2.map((iconPath) {
+          int index = navIcons2.indexOf(iconPath);
+          bool isSelected = selectedIndex == index;
+          return GestureDetector(
+            onTap: () => onNavItemTapped(index), // Update selectedIndex
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: isSelected
+                        ? const Color(0xFFE6A800)
+                        : Colors.transparent,
+                  ),
+                  child: Image.asset(
+                    iconPath,
+                    width: 30,
+                    height: 30,
+                    color: isSelected
+                        ? const Color(0xFF2F6DCF)
+                        : const Color(0xFFFFCC00),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      ),
+    );
   }
 }

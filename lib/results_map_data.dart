c65594cb1ba0e5751f60dc16750bc41eef4b44d8
AndrewZ496 <an719055@ucf.edu>
2025-03-30 14:52:46 -0400

    Fully done results page

--- lib/results_map_data.dart
+++ lib/results_map_data.dart
@@ -25,15 +25,20 @@ class VisualizedResults {
 
   /// Factory constructor to create a TestMapData object from Firebase
   static Future<VisualizedResults> fromFirebase(String testID, String collectionID, Map<String, dynamic> data) async {
-  String collectionName = collectionID.split('/').first;
-  var polygonData = <List<LatLng>>[];
-  List<List<LatLng>> additionalPolygonData = [];
-  Set<Polygon> polygonsWithColor = {};
-  List<dynamic> vegetationPolygonData = [];
-  List<dynamic> waterBodyPolygonData = [];
+    
+    String collectionName = collectionID.split('/').first;
+    //var polygonData = <List<LatLng>>[];
+    List<List<LatLng>> additionalPolygonData = [];
+    Set<Polygon> polygonsWithColor = {};
+    List<dynamic> vegetationPolygonData = [];
+    List<dynamic> waterBodyPolygonData = [];
+    List<dynamic> materialPolygonData = [];
+    List<dynamic> shelterPolygonData = [];
 
-  if (collectionName == "nature_prevalence_tests")
-  {
+    Set<Polyline> polylinesWithColor = {};
+
+    if (collectionName == "nature_prevalence_tests")
+    {
       // Extract polygon data from the nested 'design' array
       var designData = data['data']?['vegetation']?['design'];
       var nativeData = data['data']?['vegetation']?['native'];
@@ -60,32 +65,66 @@ class VisualizedResults {
 
       polygonsWithColor.addAll(_parsePolygons(vegetationPolygonData, testID, collectionName, "vegetation"));
       polygonsWithColor.addAll(_parsePolygons(waterBodyPolygonData, testID, collectionName, "waterBody"));
-  }
-  else if (collectionName == "identifying_access_tests")
-  {
-    var parkingData = data['data']?['parking'];
-    additionalPolygonData.addAll(_extractPolygonsFromDataIdentifyingAccessTests(parkingData, collectionName));
-    polygonsWithColor.addAll(_parsePolygons(additionalPolygonData, testID, collectionName, "black"));
-  }
+    }
+    else if (collectionName == "identifying_access_tests")
+    {
+      var parkingData = data['data']?['parking'];
+      additionalPolygonData.addAll(_extractPolygonsFromDataIdentifyingAccessTests(parkingData, collectionName));
+      polygonsWithColor.addAll(_parsePolygons(additionalPolygonData, testID, collectionName, "black"));
+    }
+    else if (collectionName == "spatial_boundaries_tests")
+    {
+      /// Materials
+      var concreteData = data['data']?['material']?['concrete'];
+      var deckingData = data['data']?['material']?['decking'];
+      var naturalData = data['data']?['material']?['natural'];
+      var paversData = data['data']?['material']?['pavers'];
+      var tileData = data['data']?['material']?['tile'];
 
-  // **Fetch polylines from Firebase based on the collectionIDs**
-  List<Map<String, dynamic>> fetchedPolylines = [];
+      /// Shelter
+      var canopyData = data['data']?['shelter']?['canopy'];
+      var constructedData = data['data']?['shelter']?['constructed'];
+      var furnitureData = data['data']?['shelter']?['furniture'];
+      var temporaryData = data['data']?['shelter']?['temporary'];
+      var treeData = data['data']?['shelter']?['tree'];
+      
+      /// Materials
+      materialPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(concreteData, collectionName));
+      materialPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(deckingData, collectionName));
+      materialPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(naturalData, collectionName));
+      materialPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(paversData, collectionName));
+      materialPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(tileData, collectionName));
 
-  // Fetch polylines for the Identifying Access Tests collection
-  if (collectionName == "identifying_access_tests") {
-    fetchedPolylines = await loadPathsFromFirebaseIdentifyingAccessTests(testID, collectionName);
-  } 
-  // Fetch polylines for the Spatial Boundaries Tests collection
-  else if (collectionName == "spatial_boundaries_tests") {
-    fetchedPolylines = await loadPathsFromFirebaseSpatialBoundariesTests(testID, collectionName);
-  }
-  // Fetch polylines for the People in Motion tests collection
-  else if (collectionName == "people_in_motion_tests")
-  {
-    fetchedPolylines = await loadPathsFromFirebasePeopleInMotionTests(testID, collectionName);
-  }
-  
-  //List<Map<String, dynamic>> fetchedPolylines = await loadPathsFromFirebaseIdentifyingAccessTests(testID);
+      /// Shelter
+      shelterPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(canopyData, collectionName));
+      shelterPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(constructedData, collectionName));
+      shelterPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(furnitureData, collectionName));
+      shelterPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(temporaryData, collectionName));
+      shelterPolygonData.addAll(_extractPolygonsFromDataNaturePrevalenceTests(treeData, collectionName));
+
+      polygonsWithColor.addAll(_parsePolygons(materialPolygonData, testID, collectionName, "material"));
+      polygonsWithColor.addAll(_parsePolygons(shelterPolygonData, testID, collectionName, "shelter"));
+    }
+
+    // **Fetch polylines from Firebase based on the collectionIDs**
+    List<Map<String, dynamic>> fetchedPolylines = [];
+
+    // Fetch polylines for the Identifying Access Tests collection
+    if (collectionName == "identifying_access_tests") {
+      fetchedPolylines = await loadPathsFromFirebaseIdentifyingAccessTests(testID, collectionName);
+      polylinesWithColor.addAll(_parsePolylines(fetchedPolylines, collectionName, "red"));
+    } 
+    // Fetch polylines for the Spatial Boundaries Tests collection
+    else if (collectionName == "spatial_boundaries_tests") {
+      fetchedPolylines = await loadPathsFromFirebaseSpatialBoundariesTests(testID, collectionName);
+      polylinesWithColor.addAll(_parsePolylines(fetchedPolylines, collectionName, "brown"));
+    }
+    // Fetch polylines for the People in Motion tests collection
+    else if (collectionName == "people_in_motion_tests")
+    {
+      fetchedPolylines = await loadPathsFromFirebasePeopleInMotionTests(testID, collectionName);
+      polylinesWithColor.addAll(_parsePolylines(fetchedPolylines, collectionName, "indigo"));
+    }
 
     return VisualizedResults(
       testID: testID,
@@ -95,7 +134,7 @@ class VisualizedResults {
         /// Absence of Order Test
         /// Extracting behaviorPoints
         ...(data['data']?['behaviorPoints'] as List<dynamic>? ?? []).map((behaviorPoint) {
-          var location = behaviorPoint['location'];
+          var location = behaviorPoint?['location'];
           if (location != null) {
             return LatLng(location.latitude, location.longitude);
           }
@@ -104,7 +143,7 @@ class VisualizedResults {
 
         /// Extracting maintenancePoints
         ...(data['data']?['maintenancePoints'] as List<dynamic>? ?? []).map((maintenancePoint) {
-          var location = maintenancePoint['location'];
+          var location = maintenancePoint?['location'];
           if (location != null) {
             return LatLng(location.latitude, location.longitude);
           }
@@ -114,7 +153,7 @@ class VisualizedResults {
         /// Acoustic Profile Test
         /// Extract standingPoint from dataPoints
         ...(data['data']?['dataPoints'] as List<dynamic>? ?? []).map((standingPoint) {
-          var location = standingPoint['standingPoint']?['point'];
+          var location = standingPoint?['standingPoint']?['point'];
           if (location != null) {
             return LatLng(location.latitude, location.longitude);
           }
@@ -124,7 +163,7 @@ class VisualizedResults {
         /// People in Place Test
         /// Extracting standingPoints
         ...(data['data']?['persons'] as List<dynamic>? ?? []).map((standingPoint) {
-          var location = standingPoint['location'];
+          var location = standingPoint?['location'];
           if (location != null) {
             return LatLng(location.latitude, location.longitude);
           }
@@ -186,7 +225,7 @@ class VisualizedResults {
                   return LatLng(point.latitude, point.longitude);
                 }).toList() : []),
       ]),
-      polylines: _parsePolylines(fetchedPolylines, collectionName),
+      polylines:polylinesWithColor,
       polygons: polygonsWithColor,
     );
   }
@@ -207,16 +246,30 @@ class VisualizedResults {
   }
 
   /// Generic polyline parsing function
-  static Set<Polyline> _parsePolylines(List<dynamic> polylinesData, String collectionName) {
+  static Set<Polyline> _parsePolylines(List<dynamic> polylinesData, String collectionName, String polylineColor) {
     return polylinesData.map((polyline) {
       List<LatLng> points = (polyline['points'] as List)
           .map((point) => LatLng(point['lat'], point['lng']))
           .toList();
+      
+      Color color = Color(0xFF000000);
 
+      if (collectionName == "identifying_access_tests" && polylineColor == "red")
+      {
+        color = Color.fromARGB(255, 255, 0, 0); // Solid Redcolor = Color;
+      }
+      else if (collectionName == "spatial_boundaries_tests" && polylineColor == "brown")
+      {
+        color = Color.fromARGB(255, 139, 69, 19); // Solid Brown
+      }
+      else if (collectionName == "people_in_motion_tests" && polylineColor == "indigo")
+      {
+        color = Color.fromARGB(255, 149, 0, 255); // Solid Indigo
+      }
       return Polyline(
         polylineId: PolylineId(polyline['id']),
         points: points, // List of LatLng points
-        color: Colors.red, // Set a color for the polyline
+        color: color, // Set a color for the polyline
         width: 3, // Set the width of the polyline
       );
     }).toSet();
@@ -322,13 +375,13 @@ class VisualizedResults {
       //print('Document Data: $polylineData');
 
       // Handle different categories (bikeRack, parking, etc.)
-      var categories = ['bikeRack', 'parking', 'taxiAndRideShare', 'transportStation']; // Add other categories as needed
+      var categories = ['buildingWall', 'curb', 'fench', 'partialWall', 'planter']; // Add other categories as needed
       for (var category in categories) {
-        var categoryData = polylineData['data']?[category];
+        var categoryData = polylineData['data']?['constructed']?[category];
         if (categoryData != null && categoryData is List) {
           // Loop through each path in this category
           for (var pathData in categoryData) {
-            var pathInfo = pathData['pathInfo'];
+            var pathInfo = pathData;
             if (pathInfo is String) {
               try {
                 // If it's a string, attempt to decode it as JSON
@@ -340,7 +393,7 @@ class VisualizedResults {
             }
 
             if (pathInfo is Map<String, dynamic>) {
-              List<Map<String, double>> points = (pathInfo['path'] as List).map((point) {
+              List<Map<String, double>> points = (pathInfo['polyline'] as List).map((point) {
                 if (point is GeoPoint) {
                   return {'lat': point.latitude, 'lng': point.longitude};
                 }
@@ -514,15 +567,31 @@ class VisualizedResults {
           // Assign colors based on category
           if (collectionName == "nature_prevalence_tests" && polygonColor == "vegetation") 
           {
-            strokeColor = Color.fromRGBO(0, 255, 123, 1);
-            fillColor = Color.fromRGBO(0, 255, 0, 0.2);
+            if (polygonColor == "vegetation")
+            {
+              strokeColor = Color.fromRGBO(0, 255, 123, 1);
+              fillColor = Color.fromRGBO(0, 255, 0, 0.2);
+            }
+            else if (polygonColor == "waterBody")
+            {
+              strokeColor = Color.fromRGBO(0, 0, 70, 1); // Dark blue
+              fillColor = Color.fromRGBO(0, 0, 255, 0.2); // Light blue with 20% opacity
+            }
           } 
-          else if (collectionName == "nature_prevalence_tests" && polygonColor == "waterBody") 
+          else if (collectionName == "spatial_boundaries_tests") 
           {
-            strokeColor = Color.fromRGBO(0, 0, 70, 1); // Dark blue
-            fillColor = Color.fromRGBO(0, 0, 255, 0.2); // Light blue with 20% opacity
+            if (polygonColor == "material")
+            {
+              strokeColor = Color.fromARGB(255, 128, 0, 128); // Standard Purple
+              fillColor = Color.fromARGB(50, 128, 0, 128);  // Transparent Purple Fill
+            }
+            else if (polygonColor == "shelter")
+            {
+              strokeColor = Color.fromARGB(255, 204, 85, 0); // Deep Burnt Orange
+              fillColor = Color.fromARGB(50, 204, 85, 0);  // Lighter Transparent Burnt Orange
+            }
           }
-          else if (collectionName == "identifying_access_tests" && polygonColor == "black")
+          else if (collectionName == "identifying_access_tests")
           {
             strokeColor = Color.fromARGB(255, 168, 253, 1);
             fillColor = Color.fromRGBO(0, 0, 0, 0.2);

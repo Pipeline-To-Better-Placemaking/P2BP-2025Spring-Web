e71748c32d72a32f18e772ae9f8a744ddd86d6a5
Dominic <domcasta09@gmail.com>
2025-05-12 22:03:06 -0400

    Fixed registering. Changed to use Member register function.

--- lib/Register.dart
+++ lib/Register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'db_schema_classes/member_class.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -12,13 +13,15 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _fullNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   bool _obscureText = true;
   bool _obscureConfirmText = true;
   double _scale = 1.0;
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance; // Firestore instance
+  final FirebaseFirestore _firestore =
+      FirebaseFirestore.instance; // Firestore instance
 
   // Validation feedback variables
   bool _hasUpperCase = false;
@@ -26,7 +29,7 @@ class _RegisterPageState extends State<RegisterPage> {
   bool _hasDigits = false;
   bool _hasSpecialCharacter = false;
   bool _isLengthValid = false;
-  bool _isTypingPassword = false;  // Flag to track if user has started typing
+  bool _isTypingPassword = false; // Flag to track if user has started typing
 
   @override
   void dispose() {
@@ -53,46 +56,39 @@ class _RegisterPageState extends State<RegisterPage> {
   Future<void> _registerUser() async {
     if (_formKey.currentState!.validate()) {
       try {
-        // Create user with email and password
-        UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-          email: _emailController.text,
-          password: _passwordController.text,
-        );
-
-        // Update the user's displayName with the full name entered during registration
-        await userCredential.user?.updateProfile(displayName: _fullNameController.text.trim());
- 	
-        // Add user data to Firestore
-        await _firestore.collection('users').doc(userCredential.user?.uid).set({
-          'fullName': _fullNameController.text.trim(),
-          'email': _emailController.text.trim(),
-          'creationTime': FieldValue.serverTimestamp(),
-        });
-
-        // Send email verification
-        if (userCredential.user != null) {
-          await userCredential.user!.sendEmailVerification();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                'Registration successful! A verification email has been sent to ${_emailController.text.trim()}. Please verify your email before logging in.',
-              ),
-            ),
-          );
+        if (_passwordController.text
+                .compareTo(_confirmPasswordController.text) !=
+            0) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Failed to register: passwords do not match'),
+          ));
+          return;
         }
 
-        // If registration is successful, show a message and navigate
+        final String fullName = _fullNameController.text.trim();
+        final String email = _emailController.text.trim();
+
+        await Member.createNew(fullName, email, _passwordController.text);
+
+        if (!mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Registration successful! A verification email has been sent '
+              'to $email. Please verify your email before logging in.',
+            ),
+          ),
+        );
+
+        if (!mounted) return;
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('User Registered Successfully!')),
         );
-
-        // Optionally, navigate to the login page (or another page)
-        Navigator.pop(context); // This will return to the login page, for example.
-      } catch (e) {
-        // Handle errors
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to register: $e')),
-        );
+        Navigator.pop(context);
+      } catch (e, s) {
+        print('Exception: $e');
+        print('Stacktrace: $s');
+        throw Exception('Failed to register because of exception: $e');
       }
     }
   }
@@ -112,11 +108,13 @@ class _RegisterPageState extends State<RegisterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1C48A6),
-      body: SingleChildScrollView(  // Wrap the entire body in a scroll view
+      body: SingleChildScrollView(
+        // Wrap the entire body in a scroll view
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisSize: MainAxisSize.min,  // Ensure the column doesn't take too much vertical space
+            mainAxisSize: MainAxisSize
+                .min, // Ensure the column doesn't take too much vertical space
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Center(
@@ -130,7 +128,10 @@ class _RegisterPageState extends State<RegisterPage> {
               SizedBox(height: 16),
               Text(
                 'Create an Account',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 24),
@@ -151,16 +152,24 @@ class _RegisterPageState extends State<RegisterPage> {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2.0),
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
                             ),
                             errorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             focusedErrorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             prefixIcon: Icon(Icons.person, color: Colors.white),
-                            errorStyle: TextStyle(color: Colors.white, fontSize: 18),
+                            errorStyle:
+                                TextStyle(color: Colors.white, fontSize: 18),
                           ),
                           style: TextStyle(color: Colors.white),
                           validator: (value) {
@@ -185,16 +194,24 @@ class _RegisterPageState extends State<RegisterPage> {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2.0),
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
                             ),
                             errorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             focusedErrorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             prefixIcon: Icon(Icons.email, color: Colors.white),
-                            errorStyle: TextStyle(color: Colors.white, fontSize: 18),
+                            errorStyle:
+                                TextStyle(color: Colors.white, fontSize: 18),
                           ),
                           keyboardType: TextInputType.emailAddress,
                           style: TextStyle(color: Colors.white),
@@ -202,7 +219,8 @@ class _RegisterPageState extends State<RegisterPage> {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your email';
                             }
-                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                            if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                                .hasMatch(value)) {
                               return 'Please enter a valid email address';
                             }
                             return null;
@@ -224,19 +242,29 @@ class _RegisterPageState extends State<RegisterPage> {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2.0),
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
                             ),
                             errorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             focusedErrorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             prefixIcon: Icon(Icons.lock, color: Colors.white),
-                            errorStyle: TextStyle(color: Colors.white, fontSize: 18),
+                            errorStyle:
+                                TextStyle(color: Colors.white, fontSize: 18),
                             suffixIcon: IconButton(
                               icon: Icon(
-                                _obscureText ? Icons.visibility_off : Icons.visibility,
+                                _obscureText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
                                 color: Colors.white,
                               ),
                               onPressed: _togglePasswordVisibility,
@@ -263,7 +291,11 @@ class _RegisterPageState extends State<RegisterPage> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
                               // Only show the "Password must contain:" line if any condition is unmet
-                              if (!_hasUpperCase || !_hasLowerCase || !_hasDigits || !_hasSpecialCharacter || !_isLengthValid)
+                              if (!_hasUpperCase ||
+                                  !_hasLowerCase ||
+                                  !_hasDigits ||
+                                  !_hasSpecialCharacter ||
+                                  !_isLengthValid)
                                 Text(
                                   'Password must contain:',
                                   style: TextStyle(color: Colors.white),
@@ -271,27 +303,32 @@ class _RegisterPageState extends State<RegisterPage> {
                               if (!_hasUpperCase)
                                 Text(
                                   '✘ Uppercase letter',
-                                  style: TextStyle(color: const Color(0xFFFFFFFF)),
+                                  style:
+                                      TextStyle(color: const Color(0xFFFFFFFF)),
                                 ),
                               if (!_hasLowerCase)
                                 Text(
                                   '✘ Lowercase letter',
-                                  style: TextStyle(color: const Color(0xFFFFFFFF)),
+                                  style:
+                                      TextStyle(color: const Color(0xFFFFFFFF)),
                                 ),
                               if (!_hasDigits)
                                 Text(
                                   '✘ Number',
-                                  style: TextStyle(color: const Color(0xFFFFFFFF)),
+                                  style:
+                                      TextStyle(color: const Color(0xFFFFFFFF)),
                                 ),
                               if (!_hasSpecialCharacter)
                                 Text(
                                   '✘ Special character',
-                                  style: TextStyle(color: const Color(0xFFFFFFFF)),
+                                  style:
+                                      TextStyle(color: const Color(0xFFFFFFFF)),
                                 ),
                               if (!_isLengthValid)
                                 Text(
                                   '✘ Minimum length of 12 characters',
-                                  style: TextStyle(color: const Color(0xFFFFFFFF)),
+                                  style:
+                                      TextStyle(color: const Color(0xFFFFFFFF)),
                                 ),
                             ],
                           ),
@@ -310,19 +347,29 @@ class _RegisterPageState extends State<RegisterPage> {
                               borderSide: BorderSide(color: Colors.white),
                             ),
                             focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 2.0),
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2.0),
                             ),
                             errorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             focusedErrorBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: const Color.fromARGB(255, 255, 255, 255), width: 2.0),
+                              borderSide: BorderSide(
+                                  color:
+                                      const Color.fromARGB(255, 255, 255, 255),
+                                  width: 2.0),
                             ),
                             prefixIcon: Icon(Icons.lock, color: Colors.white),
-                            errorStyle: TextStyle(color: Colors.white, fontSize: 18),
+                            errorStyle:
+                                TextStyle(color: Colors.white, fontSize: 18),
                             suffixIcon: IconButton(
                               icon: Icon(
-                                _obscureConfirmText ? Icons.visibility_off : Icons.visibility,
+                                _obscureConfirmText
+                                    ? Icons.visibility_off
+                                    : Icons.visibility,
                                 color: Colors.white,
                               ),
                               onPressed: _toggleConfirmPasswordVisibility,
@@ -343,22 +390,22 @@ class _RegisterPageState extends State<RegisterPage> {
                       ),
                     ),
                     SizedBox(height: 24),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      width: 150,
-                      child: ElevatedButton(
-                        onPressed: _registerUser,
-                        child: Text(
-                          'Register',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFFFB300),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        width: 150,
+                        child: ElevatedButton(
+                          onPressed: _registerUser,
+                          child: Text(
+                            'Register',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFFFFB300),
+                          ),
                         ),
                       ),
                     ),
-                  ),
                     SizedBox(height: 24),
                     Center(
                       child: Column(
@@ -370,12 +417,14 @@ class _RegisterPageState extends State<RegisterPage> {
                           SizedBox(height: 8),
                           TextButton(
                             onPressed: () {
-                              Navigator.pop(context); // Navigate back to the login page
+                              Navigator.pop(
+                                  context); // Navigate back to the login page
                             },
                             child: Text(
                               'Login here',
                               style: TextStyle(
-                                color: Color.fromARGB(255, 255, 255, 255), // Yellow color text
+                                color: Color.fromARGB(
+                                    255, 255, 255, 255), // Yellow color text
                                 fontSize: 16,
                               ),
                             ),
